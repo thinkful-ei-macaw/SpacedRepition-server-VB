9aba8df2a37a22e3424950b97465230f9c8d721b
Brannen Petit <bpetit940@gmail.com>
2020-05-20 11:45:02 -0500

    started guess endpont

--- src/language/language-router.js
+++ src/language/language-router.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const LanguageService = require("./language-service");
 const { requireAuth } = require("../middleware/jwt-auth");
+const jsonBodyParser = express.json();
 
 const languageRouter = express.Router();
 
@@ -40,12 +41,13 @@ languageRouter.get("/", async (req, res, next) => {
   }
 });
 
-languageRouter.get("/head", async (req, res, next) => {
+languageRouter.get("/head", requireAuth, async (req, res, next) => {
   try {
     const nextWord = await LanguageService.getLanguageWords(
       req.app.get("db"),
       req.language.id
     );
+
     res.json({
       nextWord: nextWord[0].original,
       totalScore: req.language.total_score,
@@ -58,9 +60,48 @@ languageRouter.get("/head", async (req, res, next) => {
   }
 });
 
-languageRouter.post("/guess", async (req, res, next) => {
-  // implement me
-  res.send("implement me!");
-});
+languageRouter.post(
+  "/guess",
+  jsonBodyParser,
+  requireAuth,
+  async (req, res, next) => {
+    if (!req.body.guess) {
+      res.status(400).send({ error: "Missing 'guess' in request body" });
+    }
+    try {
+      const nextWord = await LanguageService.getLanguageWords(
+        req.app.get("db"),
+        req.language.id
+      );
+      const wordsList = LanguageService.createWordList(nextWord);
+      console.log(wordsList);
+      if (req.body.guess !== wordsList.head.value.translation) {
+        res.json({
+          nextWord: wordsList.head.next.value.original,
+          totalScore: req.language.total_score,
+          wordCorrectCount: wordsList.head.value.correct_count,
+          wordIncorrectCount: wordsList.head.value.incorrect_count++,
+          answer: wordsList.head.value.translation,
+          isCorrect: false,
+        });
+
+        res.status(200).send();
+        next();
+      }
+      if (req.body.guess === nextWord[0].translation) {
+        res.json({
+          nextWord: nextWord[1].original,
+          totalScore: req.language.total_score,
+          wordCorrectCount: nextWord[0].correct_count,
+          wordIncorrectCount: nextWord[0].incorrect_count,
+          answer: nextWord[0].translation,
+          isCorrect: true,
+        });
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 module.exports = languageRouter;
--- src/language/language-service.js
+++ src/language/language-service.js
@@ -1,33 +1,51 @@
+const LinkedList = require("../linked-list/linked-list");
+
 const LanguageService = {
   getUsersLanguage(db, user_id) {
     return db
-      .from('language')
+      .from("language")
       .select(
-        'language.id',
-        'language.name',
-        'language.user_id',
-        'language.head',
-        'language.total_score',
+        "language.id",
+        "language.name",
+        "language.user_id",
+        "language.head",
+        "language.total_score"
       )
-      .where('language.user_id', user_id)
-      .first()
+      .where("language.user_id", user_id)
+      .first();
   },
 
   getLanguageWords(db, language_id) {
     return db
-      .from('word')
+      .from("word")
       .select(
-        'id',
-        'language_id',
-        'original',
-        'translation',
-        'next',
-        'memory_value',
-        'correct_count',
-        'incorrect_count',
+        "id",
+        "language_id",
+        "original",
+        "translation",
+        "next",
+        "memory_value",
+        "correct_count",
+        "incorrect_count"
       )
-      .where({ language_id })
+      .where({ language_id });
   },
-}
+  createWordList(nextWerd) {
+    const sll = new LinkedList();
+    // //if any items in ll
 
-module.exports = LanguageService
+    // let currentNode = sll.head;
+    // let previousNode = sll.head;
+    // let someArray = []
+    // while(currentNode.next !== null){
+    //   previousNode
+    // }
+
+    for (let i = nextWerd.length - 1; i >= 0; i--) {
+      sll.insertFirst(nextWerd[i]);
+    }
+    return sll;
+  },
+};
+
+module.exports = LanguageService;
